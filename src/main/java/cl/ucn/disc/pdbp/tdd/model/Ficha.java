f602ea191d1017b1e3561b4380b4891e625fa736
Fabian Rojas <fnra027@gmail.com>
2020-05-06 10:56:27 -0400

    feat(java): Creacion de la clase

---
+++ src/main/java/cl/ucn/disc/pdbp/tdd/model/Ficha.java
@@ -0,0 +1,18 @@
+package cl.ucn.disc.pdbp.tdd.model;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author Fabian Rojas
+ */
+public class Ficha {
+
+    private final int numFicha;
+    private final String nombre;
+    private final String especie;
+    private final ZonedDateTime fechaNacimiento;
+    private final String raza;
+
+
+
+}
